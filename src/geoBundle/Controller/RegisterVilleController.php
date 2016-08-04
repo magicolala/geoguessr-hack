8f0fc962643d7bdd8c154f4b70f711154e691600
Cédric Oloa <cedric.oloa@laposte.net>
2016-08-04 18:25:43 +0200

    aléatoire

--- src/geoBundle/Controller/RegisterVilleController.php
+++ src/geoBundle/Controller/RegisterVilleController.php
@@ -9,14 +9,26 @@ class RegisterVilleController extends Controller
 {
     public function registerAction()
     {
-        $ville = new Ville();
-
-        $ville->setName('Cannes');
-        $ville->setLat('43.54701719603172');
-        $ville->setLon('7.031603886799303');
-
         $em = $this->getDoctrine()->getManager();
-        $em->persist($ville);
+
+        $cannes = new Ville();
+        $cannes->setName('Cannes');
+        $cannes->setLat('43.54701719603172');
+        $cannes->setLon('7.031603886799303');
+        $em->persist($cannes);
+
+        $versailles = new Ville();
+        $versailles->setName('Versailles');
+        $versailles->setLat('48.80262840351376');
+        $versailles->setLon('2.1287524621873217');
+        $em->persist($versailles);
+
+        $laroche = new Ville();
+        $laroche->setName('La-Roche-Sur-Yon');
+        $laroche->setLat('46.67048263549805');
+        $laroche->setLon('-1.4269371032714844');
+        $em->persist($laroche);
+
         $em->flush();
 
         return $this->render('geoBundle:Default:index.html.twig');
