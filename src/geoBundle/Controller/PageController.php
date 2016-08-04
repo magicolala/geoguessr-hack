e324c1697f0b7ad4163023cdcb6c77035cec5774
Cédric Oloa <cedric.oloa@laposte.net>
2016-08-04 23:06:00 +0200

    position reponse

--- src/geoBundle/Controller/PageController.php
+++ src/geoBundle/Controller/PageController.php
@@ -8,19 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
-    public function get_distance_m($lat1, $lng1, $lat2, $lng2) {
-      $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
-      $rlo1 = deg2rad($lng1);
-      $rla1 = deg2rad($lat1);
-      $rlo2 = deg2rad($lng2);
-      $rla2 = deg2rad($lat2);
-      $dlo = ($rlo2 - $rlo1) / 2;
-      $dla = ($rla2 - $rla1) / 2;
-      $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo
-    ));
-      $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
-      return ($earth_radius * $d);
-    }
+
 
     public function settrAction()
     {
@@ -43,24 +31,40 @@ class PageController extends Controller
 
     public function gameReponseAction(Request $request)
     {
-        $reponse = $request->request->get('position')
-        $villeLatStr = $request->request->->get('ville_lat');
-        $villeLonStr = $request->request->->get('ville_lng');
+        function get_distance_m($lat1, $lng1, $lat2, $lng2) {
+          $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
+          $rlo1 = deg2rad($lng1);
+          $rla1 = deg2rad($lat1);
+          $rlo2 = deg2rad($lng2);
+          $rla2 = deg2rad($lat2);
+          $dlo = ($rlo2 - $rlo1) / 2;
+          $dla = ($rla2 - $rla1) / 2;
+          $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo
+        ));
+          $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+          return ($earth_radius * $d);
+        }
+        $reponse = $request->request->get('position');
+        $villeLatStr = $request->request->get('ville_lat');
+        $villeLonStr = $request->request->get('ville_lng');
 
         $pattern = '/(\d+\.\d+)/';
         $test = preg_match_all($pattern, $reponse, $matches);
 
-        $positionLonStr = $matches[0][0];
-        $positionLatStr = $matches[0][1];
+        $positionLatStr = $matches[0][0];
+        $positionLonStr = $matches[0][1];
 
-        $positionLon = intval($matches[0][0]);
-        $positionLat = intval($matches[0][1]);
-        $villeLat = intval($villeLatStr);
-        $villeLon = intval($villeLonStr);
+        $positionLat = floatval($matches[0][0]);
+        $positionLon = floatval($matches[0][1]);
+
+
+        $villeLat = floatval($villeLatStr);
+        $villeLon = floatval($villeLonStr);
         $em = $this->getDoctrine()->getManager();
         $ville = $em->getRepository('geoBundle:Ville')->findOneRandom();
 
-        $distance = round(get_distance_m($positionLon, $positionLat, $villeLat, $villeLon) / 1000, 3);
+        $distance = round(get_distance_m($positionLat, $positionLon, $villeLat, $villeLon) / 1000, 3);
+        var_dump($distance); die;
         return $this->render('geoBundle:Default:game.html.twig', array(
             'ville' => $ville,
             'distance' => $distance
