9f1583c95afa56fafb7c0ffb0c3998a338722173
PTony <jokr.01@gmail.com>
2016-08-05 02:28:18 +0200

    generation entity user

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace UserBundle\Entity;
+
+/**
+ * User
+ */
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $useername;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set useername
+     *
+     * @param string $useername
+     *
+     * @return User
+     */
+    public function setUseername($useername)
+    {
+        $this->useername = $useername;
+
+        return $this;
+    }
+
+    /**
+     * Get useername
+     *
+     * @return string
+     */
+    public function getUseername()
+    {
+        return $this->useername;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
+
